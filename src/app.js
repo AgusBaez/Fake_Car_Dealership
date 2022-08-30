e9a1bc88032c81122c53f0c0d6ac92d247b49aa2
AgusBaez <agustinbaezignacio@gmail.com>
2022-08-30 16:39:25 -0300

    Create new Middleware Date

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@ import express from "express";
 //importo rutas
 import productsRouter from "./routes/products.routes.js";
 import categoriesRouter from "./routes/categories.routes.js";
-
+//Importo middlewares
+import {dateToDay} from "./middleware/dates.js";
 //funcionalidades Express
 const app = express();
 
@@ -11,7 +12,7 @@ const app = express();
 app.use(express.json()); //middleware de express.json(): el servidor interpreta todo tipo de dato JSON para guardarlo: estos json viene en los endpoints req
 
 
-//router
-app.use(productsRouter, categoriesRouter);
+//Middleware & Router
+app.use(dateToDay, productsRouter, categoriesRouter);
 
 export default app;
---
+++ src/middleware/dates.js
@@ -0,0 +1,24 @@
+//VIDEO PRACTICO: - https://www.youtube.com/watch?v=aIHQsAjRLYU
+// DOCUMENTACION: - https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Date
+
+//Methodos del date;
+//console.log(Date());// Prototipo de una funcion en codigo nativo
+//console.log(date.getDate(), date.getDay(), date.getMonth(), date.getFullYear(), date.getHours(), date.getMinutes(), date.getSeconds, date.getMilliseconds);
+//console.log(date.toString());
+//console.log(date.toDateString());
+//console.log(date.getTimezoneOffset); Uso horario del pais
+//console.log(date.toUTCString());//
+//console.log(Date.now());//TimeStamp, cuantos segundos pasaron desde 1° de enero de 1970
+
+//LIBRERIA: MOMENT.js - para el menejo del tiempo
+
+export function dateToDay(req, res, next) {
+  let date = new Date();
+
+  return (
+    console.log(
+      `new Request - ${req.method} - On ${date.toLocaleDateString()}`
+    ),
+    next()
+  );
+}
