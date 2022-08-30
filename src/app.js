bce0ba5cdcf7e8beb35446be1f75d7e6bc4e4e07
AgusBaez <agustinbaezignacio@gmail.com>
2022-08-30 18:21:07 -0300

    Modified app.js New ErrorHanddler.js

--- src/app.js
+++ src/app.js
@@ -4,15 +4,12 @@ import express from "express";
 import productsRouter from "./routes/products.routes.js";
 import categoriesRouter from "./routes/categories.routes.js";
 //Importo middlewares
-import {dateToDay} from "./middleware/dates.js";
+import { dateToDay } from "./middleware/dates.js";
 //funcionalidades Express
 const app = express();
 
-//middlewares
-app.use(express.json()); //middleware de express.json(): el servidor interpreta todo tipo de dato JSON para guardarlo: estos json viene en los endpoints req
-
-
 //Middleware & Router
+app.use(express.json()); //middleware de express.json(): el servidor interpreta todo tipo de dato JSON para guardarlo: estos json viene en los endpoints req
 app.use(dateToDay, productsRouter, categoriesRouter);
 
 export default app;
---
+++ src/middleware/errorHandler.js
@@ -0,0 +1,7 @@
+//Video ErrorHandler - https://www.youtube.com/watch?v=x7CW7ex37OM&list=PLJPvCr6dK-cmuvG2kErSLKgahMnIihmTY&index=15
+//Brechas de seguridad & Programacion Defensiva
+//Error_Unauthorized = 401 - unauthorized
+//Error_Not_Found = 404
+//Error_Timeout = 408
+//Error_Server 500
+//Error_Service_Unavailable = 503
