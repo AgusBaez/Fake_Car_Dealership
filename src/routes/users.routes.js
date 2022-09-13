0727fab2862289415333272f2c20ca7fb3d57c9c
AgusBaez <agustinbaezignacio@gmail.com>
2022-09-13 15:53:08 -0300

    :sparkles: User C.R.U.D Router

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -8,6 +8,9 @@ const errorHanddler = require("../middleware/errorHandler");
 
 router.get("/users", usersController.getUsers);
 router.post("/users", usersController.addUser);
+router.get("/users/:id", usersController.getUsersById);
+router.put("/users/:id", usersController.updateUser);
+router.delete("/users/:id", usersController.deleteUser);
 //Se generea un error si no encuentra rutas
 router.use(errorHanddler.notFound);
 
