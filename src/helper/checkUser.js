80b9cff9c074083e4b4f9293508388f5b631e38c
AgusBaez <agustinbaezignacio@gmail.com>
2022-09-13 15:55:58 -0300

    :sparkles: Add Helper CheckEmailUser

---
+++ src/helper/checkUser.js
@@ -0,0 +1,13 @@
+const userMailValidator = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+
+module.exports = {
+  userMailValidator,
+};
+
+//match devuelve null si no se cumple la concicion, si cumple devuelve TRUE
