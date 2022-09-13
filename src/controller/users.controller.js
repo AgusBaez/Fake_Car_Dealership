dc33f7efe8bd1a567c841e881a49fb8e004104ff
AgusBaez <agustinbaezignacio@gmail.com>
2022-09-13 15:54:27 -0300

    :sparkles: User Controller

--- src/controller/users.controller.js
+++ src/controller/users.controller.js
@@ -1,5 +1,6 @@
 const db = require("../models/index");
 const { User } = db;
+const helper = require("../helper/checkUser");
 
 const getUsers = async (_req, res, next) => {
   await User.findAll() //RECORRE LAS TUPLAS DE LA TABLA Y CREA UN ARREGLO
@@ -9,9 +10,34 @@ const getUsers = async (_req, res, next) => {
     .catch((error) => next(error));
 };
 
+const getUsersById = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const userFind = await User.findOne({
+      where: {
+        id,
+      },
+    });
+    if (userFind === null) {
+      res.status(400).send("Not Found User");
+    } else {
+      res.status(201).send(userFind);
+    }
+  } catch (error) {
+    return next(error);
+  }
+};
+
 const addUser = async (req, res, next) => {
   try {
     const { firstName, lastName, email } = req.body;
+    //helper checkMail
+    if (!helper.userMailValidator(req.body.email)) {
+      const error = new Error(" CONFLICT MAIL (409)");
+      error.status = 409;
+      next(error);
+    }
+
     const newUser = await User.create({
       firstName,
       lastName,
@@ -23,9 +49,45 @@ const addUser = async (req, res, next) => {
   }
 };
 
+const updateUser = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const incomingBody = req.body;
+
+    if (!helper.userMailValidator(req.body.email)) {
+      const error = new Error(" CONFLICT MAIL (409)");
+      error.status = 409;
+      next(error);
+    }
+    
+    await User.update(incomingBody, {
+      where: { id },
+    });
+    res.status(201).send(`Updated User ID: ${id}`);
+  } catch (error) {
+    return next(error);
+  }
+};
+
+const deleteUser = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+
+    await User.destroy({
+      where: { id },
+    });
+    res.status(202).send(`Deleted User ID: ${id}`);
+  } catch (error) {
+    return next(error);
+  }
+};
+
 const usersController = {
   getUsers,
+  getUsersById,
   addUser,
+  updateUser,
+  deleteUser,
 };
 
 module.exports = usersController;
