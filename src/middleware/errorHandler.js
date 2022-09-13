0cd55930f58b9f3571d1e5be8eac0b9af2254df0
AgusBaez <agustinbaezignacio@gmail.com>
2022-09-13 15:55:26 -0300

    :zap: Update Middleware ErrorHandler

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,21 +1,27 @@
 //Brechas de seguridad & Programacion Defensiva
-//Error_Unauthorized = 401 - unauthorized
+//Error_Unauthorized = 401
 //Error_Not_Found = 404
 //Error_Timeout = 408
+//Conflict = (409)
 //Error_Server 500
 //Error_Service_Unavailable = 503
+
+//En caso de haber algun error en los endpoints mostralo y continua
 const errorLogger = (error, req, res, next) => {
   console.error(error);
   next(error);
 };
 
+//Segun los estados de error atrapar y mostrar el error
 const errorParser = (error, req, res, next) => {
   if (error.status === 404) {
     //PODRIA REDIRRECCIONAR A UN ENDPOINT 404
     res.status(404).send("NOT FOUND (404)");
+  } else if (error.status === 409) {
+    res.status(409).send("- Conflict (409) Invalid Mail - ");
   } else if (error.errors.find((err) => err.type === "notNull Violation")) {
-    //si en los errores(Objeto error) se obtiene(encuentra) un error de "notNull" entonces envia un 400
-    res.status(400).send("- Bad Request (400) -");
+    //si en los errores(Objeto error) se obtiene(encuentra) un error de "notNull" entonces envia un 409
+    res.status(409).send("- Conflict (409) Invalid Mail - ");
   } else {
     res.status(500).send("- Server Error -");
   }
