c920869c66781e1e2a13420cb2c78d66349c916d
AgusBaez <agustinbaezignacio@gmail.com>
2022-09-22 11:31:59 -0300

    :sparkles: Car Migration

---
+++ src/migrations/20220914142522-create-cars.js
@@ -0,0 +1,40 @@
+"use strict";
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("cars", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      brand: {
+        type: Sequelize.STRING,
+      },
+      speed: {
+        type: Sequelize.INTEGER,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        defaultValue: null,
+        reference: {
+          model: "users",
+          key: "id",
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("cars");
+  },
+};
