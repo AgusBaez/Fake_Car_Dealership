704f63214a1b5fd30884960df07759be53a97fdb
AgusBaez <agustinbaezignacio@gmail.com>
2022-09-22 11:27:10 -0300

    :sparkles: Car Model

---
+++ src/models/Car.js
@@ -0,0 +1,30 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class cars extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      cars.belongsTo(models.users, {
+        foreignKey: "id",
+        target_key: "user_id",
+      });
+    }
+  }
+  cars.init(
+    {
+      brand: DataTypes.STRING,
+      speed: DataTypes.INTEGER,
+      user_id: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "cars",
+    }
+  );
+  return cars;
+};
