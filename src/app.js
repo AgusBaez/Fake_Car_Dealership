2fe4afd8bc3ce82f5baca69256ddc253cb8d6258
AgusBaez <agustinbaezignacio@gmail.com>
2022-09-24 14:46:32 -0300

    :fire: Removing code

--- src/app.js
+++ src/app.js
@@ -1,7 +1,5 @@
 //env
 require("dotenv").config();
-// JWT
-const jwt = require("jsonwebtoken");
 //config express
 const express = require("express");
 //importo rutas
@@ -18,63 +16,17 @@ const {
   notFound,
 } = require("./middleware/errorHandler");
 
-//const verification = express.Router();
-// # Auth0 #
-//const { auth, requiresAuth } = require("express-openid-connect");
-
-//middleware de verificacion del token
-//Auth Token
-// verification.use((req, res, next) => {
-//   let token = req.headers["x-access-token"] || req.headers["authorization"];
-//   if (!token) {
-//     res.status(401).send("!Unauthorized¡");
-//     return;
-//   }
-//   if (token.startsWith("Bearer ")) {
-//     token = token.slice(7, token.length);
-//     console.log(token);
-//   }
-//   if (token) {
-//     jwt.verify(token, app.get("key"), (error, decoded) => {
-//       if (error) {
-//         return res.status(401).send({ error: "invalid token" });
-//       } else {
-//         req.decoded = decoded;
-//         next();
-//       }
-//     });
-//   }
-// });
-
-// app.get("/info", verification, (req, res) => {
-//   res.status(200).send("RESPONSE VERIFICATION CHECK");
-// });
-
 //Middleware & Router
 app.use(express.json()); //middleware de express.json(): el servidor interpreta todo tipo de dato JSON para guardarlo: estos json viene en los endpoints req
-
-// JWT
-// app.set("key", process.env.KEY_JWT);
-// app.post("/login", (req, res) => {
-//   // if (verificar que el user y la pass en la DB sean iguales a lo que llega por req.body) {} else {'Unauthorized'}
-//   if (req.body.user == "admin" && req.body.password == 123) {
-//     const payload = {
-//       check: true,
-//     };
-//     const token = jwt.sign(payload, app.get("key"), {
-//       //opciones de jwt sesions
-//       expiresIn: "7d", //expiracion
-//     });
-//     res.status(201).send({ token: token });
-//   } else {
-//     res.status(401).send("Unauthorized");
-//   }
-// });
 
 app.use(dateToDay, sesionRouter, carsRouter, usersRouter);
 //Si se encuentra un error:
 app.use([notFound, errorLogger, errorParser]);
 
+module.exports = app;
+
+// # Auth0 #
+//const { auth, requiresAuth } = require("express-openid-connect");
 // # Auth0 #
 // const config = {
 //   authRequired: false,
@@ -96,5 +48,3 @@ app.use([notFound, errorLogger, errorParser]);
 // app.get("/profile", requiresAuth(), (req, res) => {
 //   res.send(JSON.stringify(req.oidc.user));
 // });
-
-module.exports = app;
